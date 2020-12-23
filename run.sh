dd5754cb7f9be9d870155d6385440dc3fa29d33f
Thales Rosa <thalescr@protonmail.com>
2020-12-23 13:03:14 -0300

    Revert to First Commit

--- run.sh
+++ run.sh
@@ -8,19 +8,11 @@ if (($EUID != 0)); then
     exit
 fi
 
-# Check positional parameters
-if (($# != 1)); then
-  echo "Usage: ./run.sh [SITE_NAME]"
-  exit
-fi
-
-site_name=$1
-
 # Get current dir
 current_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 # Docker_run command
-docker_run=""
+docker_run="docker run -d -p 80:8080 --name static-server"
 
 # Declare generate location file function
 function generate_location_file () {
@@ -41,11 +33,15 @@ for volume in $volumes; do
         location=${volume%"_static"}
         generate_location_file $location
 
-        echo "Configured to serve volume $volume in URL $site_name/$location"
+        # Append "-v" parameter to the docker run command
+        docker_run="${docker_run} -v $volume:/vol/$location"
+        echo "Configured to serve volume $volume in URL /$location"
     fi
 done
 
-# Finally download the docker image and run it
-docker run -d --name static-server -e VIRTUAL_HOST=$site_name -e LETSENCRYPT_HOST=$site_name -v $current_dir/locations:/static flashspys/nginx-static
+# Finally build the docker image and run it
+docker build --tag static-server .
+echo $docker_run
+$docker_run static-server
 
 echo "Successfully started container static-server"
